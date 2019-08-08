775dc931badf3163fa6e05f3338f570d14536786
sarab <sarabsingh212@gmail.com>
2019-08-08 17:43:14 +0530

    Redirecting to dashboard if user already logged in

--- src/containers/Home/Home.container.js
+++ src/containers/Home/Home.container.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import {Card, List, Button} from 'antd';
+import { isEmpty, withFirebase, isLoaded } from "react-redux-firebase";
+import { compose } from "redux";
+import { connect } from "react-redux";
 import CommonLayout from '../../layouts/common';
 class Home extends React.Component {
     render = () => {
-        return (
-            <CommonLayout auth={false} >
-                <Card title="Welcome to Gym Tracking app">
-                    <Link to="/login"><Button>Click here to Login</Button></Link>
-                </Card>
-            </CommonLayout>
-        )
+        const { auth } = this.props;
+        if (isLoaded(auth)) {
+            return isEmpty(auth) ? (
+                <CommonLayout auth={false} >
+                    <Card title="Welcome to Gym Tracking app">
+                        <Link to="/login"><Button>Click here to Login</Button></Link>
+                    </Card>
+                </CommonLayout>
+            ) : (
+                <Redirect to="/dashboard" />
+            )
+        }
+        return <p>Loading ...</p>
     }
 };
 
-export default Home;
+export default compose(
+    withFirebase,
+    connect(({ firebase: { auth } }) => ({ auth })))(Home);
