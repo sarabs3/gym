125c5818dffc5a17f36d8f18e546015979cfeaab
sarab <sarabsingh212@gmail.com>
2019-08-08 15:39:31 +0530

    Add login button to homepage

--- src/containers/Home/Home.container.js
+++ src/containers/Home/Home.container.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Card, List} from 'antd';
+import {Card, List, Button} from 'antd';
+import CommonLayout from '../../layouts/common';
 class Home extends React.Component {
     render = () => {
         return (
-            <Card title="Workout Plans">
-                <List
-                    dataSource={[{ title: "Plan 1" }]}
-                    renderItem={(item) => {
-                        return (
-                            <Link to="/plans/1">
-                                <List.Item>
-                                    <List.Item.Meta
-                                        title={item.title}
-                                    />
-                                </List.Item>
-                            </Link>
-                    )}}
-                />
-            </Card>
+            <CommonLayout auth={false} >
+                <Card title="Welcome to Gym Tracking app">
+                    <Link to="/login"><Button>Click here to Login</Button></Link>
+                </Card>
+            </CommonLayout>
         )
     }
 };
